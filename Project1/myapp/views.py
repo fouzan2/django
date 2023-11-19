febbdf2bd5d4c3b0c7df48be1aeb55de921ac76c
fouzan2 <63598800+fouzan2@users.noreply.github.com>
2023-11-19 15:04:59 +0500

    Gpt Integration

--- Project1/myapp/views.py
+++ Project1/myapp/views.py
@@ -1,19 +1,34 @@
 from django.shortcuts import render
 from .models import Person
+from openai import OpenAI
+
+client = OpenAI(api_key="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")
+
 
 # Create your views here.
-
+def gpt_process(string_value):
+    completion = client.chat.completions.create(
+        model="gpt-3.5-turbo",
+        messages=[
+            {"role": "user", "content": string_value},
+            {"role": "system", "content": "You are a helpful assistant."},
+        ],
+    )
+    return str(completion.choices[0].message.content)
 
 
 def welcome(request):
     result = None
 
     if request.method == "POST":
-        mynum = str(request.POST["text"])
-        result = mynum 
- 
-        myinstance = Person(userinputvalue=mynum, mycalcvalue=result)
-        myinstance.save()
+        user_input_text = str(request.POST["text"])
+        try:
+            gpt_output = gpt_process(user_input_text)
+            result = gpt_output
+            myinstance = Person(userinputvalue=user_input_text, mycalcvalue=gpt_output)
+            myinstance.save()
+        except:
+            result = "Something went wrong. Please try again!"
 
     return render(request, "index.html", {"result": result})
 
